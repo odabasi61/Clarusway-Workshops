07803eb8ed711d8763e01e2ffac076b6ac83cfff
odabasi61 <aliodabas_a@hotmail.com>
2022-12-20 20:57:40 +0300

    some codes are corrected

--- Javascript/Digit Place/script.js
+++ Javascript/Digit Place/script.js
@@ -2,15 +2,15 @@ let num = Number(
   prompt("Please enter an number and see how many digit place it has.")
 );
 
-if (!isNaN(num)) {
+if (!isNaN(num) && num > 0) {
   function countDigits(num) {
     // Convert the number to a string
     let str = num.toString();
     // Return the length of the string
     return str.length;
   }
-
   console.log(`The number you entered has ${countDigits(num)} digits.`);
-} else {
-  console.log("Please enter only numbers.");
+} 
+else {
+  console.log("Please enter only positive numbers.");
 }
--- Javascript/Find the Number/script.js
+++ Javascript/Find the Number/script.js
@@ -1,11 +1,7 @@
 let enterNumber = Number(prompt("Please enter a number between 0 and 100."));
 
-while (true) {
-  if (isNaN(enterNumber) || enterNumber < 0 || enterNumber > 100) {
+while (isNaN(enterNumber) || enterNumber < 0 || enterNumber > 100) {
     console.log("Please enter a digit between 0 and 100.");
     enterNumber = Number(prompt("Please enter a number between 0 and 100."));
-  } else {
-    console.log(`Your guess (${enterNumber}) is between 0 and 100. Congrats!`);
-    break;
-  }
-}
+}  
+console.log(`Your guess (${enterNumber}) is between 0 and 100. Congrats!`);
